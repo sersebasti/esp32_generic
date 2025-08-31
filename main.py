3a205d87bc8326cb75b87d94d06f05bc0eb8bee0
sersebasti <ser.sebastiani@gmail.com>
2025-08-31 11:24:34 +0200

    sempre relativa modfica wifi manger

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from wifi_manager import WiFiManager
 log = RollingLogger(path="log.txt", max_bytes=8*1024, backups=1, echo=True)
 
 if __name__ == "__main__":
-    mgr = WiFiManager(logger=log, wifi_json="wifi.json", check_interval=30, heartbeat=True)
+    mgr = WiFiManager(log=log, wifi_json="wifi.json", check_interval=30, heartbeat=True)
     mgr.run()
