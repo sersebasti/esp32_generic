f3419057d84ee2d01d31b378fc9c2685898535b4
sersebasti <ser.sebastiani@gmail.com>
2025-09-02 12:29:17 +0200

    comem to

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from wifi_manager import WiFiManager
 log = CircularLogger(path="log.txt", max_bytes=8*1024, echo=True)
 
 if __name__ == "__main__":
-    mgr = WiFiManager(log=log, wifi_json="wifi.json")   # ← GPIO collegato al pulsante (active-low)
+    mgr = WiFiManager(log=log, wifi_json="wifi.json")
     mgr.run()
